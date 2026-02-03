ca7d95d9900842792a01788629418fd768253707
Nalla <nallaperumal@gmail.com>
2026-02-03 16:50:44 +0530

    refactored the method names

--- AppServerCall.py
+++ AppServerCall.py
@@ -2,21 +2,39 @@ import requests
 import json
 
 class DataUpdate:
-    def makeRequest(self, url:str) -> str:
-        # url = "https://jsonplaceholder.typicode.com/posts/"
-        payload = json.dumps({
-        "title": "foo",
-        "body": "bar",
-        "userId": 1
-        })
+    def AddData(self, params) -> str:
+        url = "https://jsonplaceholder.typicode.com/posts/"
+        payload = json.dumps(params)
         headers = {
         'Content-Type': 'application/json'
         }
-
         response = requests.request("POST", url, headers=headers, data=payload)
         # print(response.text)
         return response.text
+    def UpdateData(self, params) -> str:
+        url = "https://jsonplaceholder.typicode.com/posts/"
+        payload = json.dumps(params)
+        headers = {
+        'Content-Type': 'application/json'
+        }
+        response = requests.request("PUT", url, headers=headers, data=payload)
+        # print(response.text)
+        return response.text
 
+class MongoDataUpdate:
+    def AddData(self, param):
+        print("insert data into mongo")
+    def UpdateData(self, param):
+        print("update data into mongo")    
 dataUpdate = DataUpdate()
-res = dataUpdate.makeRequest("https://jsonplaceholder.typicode.com/posts/")
+# dataUpdate = MongoDataUpdate()
+paramsToInsert =  {
+        "title": "foo",
+        "body": "bar",
+        "userId": 1
+        }
+res = dataUpdate.AddData(paramsToInsert)
+print(res)
+res = dataUpdate.UpdateData(paramsToInsert)
+# res = dataUpdate.DeleatData(2)
 print(res)
