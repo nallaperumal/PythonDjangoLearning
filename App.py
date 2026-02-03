83158d80e3cb1e6b5702add10fb41bffa9f9b13f
Nalla <nallaperumal@gmail.com>
2026-02-03 11:21:10 +0530

    implemented the abstract method

--- App.py
+++ App.py
@@ -1,24 +1,33 @@
 import time 
-
-class Travel:
+from abc import ABC, abstractmethod
+class Travel(ABC):
+    @abstractmethod
+    def LastStop():
+        pass
     def drive(self):
-        for stop in self.stops:
+        for stop in self._stops:
             print(f"...next stop is {stop}")
             time.sleep(1.2)
+        self.LastStop()    
 class Bus19(Travel):
     def __init__(self):
-        self.stops = ["stop1", "stop2", "stop3"]
+        self._stops = ["stop1", "stop2", "stop3"]
+    def LastStop(self):
+        print("last stop is stop3")
 class BusMA(Travel):
     def __init__(self):
-        self.stops = ["stopA", "stopB", "stopC"]
-
+        self._stops = ["stopA", "stopB", "stopC"]
+    def LastStop(self):
+        print("last stop is stopC")
 class BlueMetro(Travel):
     def __init__(self):
-        self.stops = ["Wimco nagar", "nandanam", "Airport"]
+        self._stops = ["Wimco nagar", "nandanam", "Airport"]
     def drive(self): 
         print("doors will open on the left side")
         super().drive()
         print("doors are closing..")
+    def LastStop(self):
+        print("last stop is Airport")    
 myBus = Bus19()
 myBus.drive()
 myMetro = BlueMetro()
