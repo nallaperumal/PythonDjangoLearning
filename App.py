f2578677ed60867bc4bb058c901f763c84b905ee
Nalla <nallaperumal@gmail.com>
2026-02-03 10:38:59 +0530

    included dynamic polymorphism

--- App.py
+++ App.py
@@ -1,17 +1,28 @@
 import time 
-from BusFactory import BusFactory
 
-# busFact = BusFactory()
-# busFact.CreateBus("19")
+class Travel:
+    def drive(self):
+        for stop in self.stops:
+            print(f"...next stop is {stop}")
+            time.sleep(1.2)
+class Bus19(Travel):
+    def __init__(self):
+        self.stops = ["stop1", "stop2", "stop3"]
+class BusMA(Travel):
+    def __init__(self):
+        self.stops = ["stopA", "stopB", "stopC"]
 
-bus19 = BusFactory.CreateBus("19")
-bus19.drive()
-
-maabus = BusFactory.CreateBus("MAA2")
-maabus.drive()
-
-# aReal19RandonBus = bus19()
-# aReal19RandonBus.drive()
-# print("....")
-# maa2Bus = busMAA2()
-# maa2Bus.drive()
+class BlueMetro(Travel):
+    def __init__(self):
+        self.stops = ["Wimco nagar", "nandanam", "Airport"]
+    def drive(self): 
+        print("doors will open on the left side")
+        super().drive()
+        print("doors are closing..")
+myBus = Bus19()
+myBus.drive()
+myMetro = BlueMetro()
+print("---------")
+myMetro.drive()
+# myBus.__Test1() #will not work
+# myBus._TestProtected() #will not work
