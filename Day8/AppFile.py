13e1f10776fddad265a1f26f08b20b04223cc7b7
Nalla <nallaperumal@gmail.com>
2026-02-04 15:36:02 +0530

    modified the hard-coded string to a variable

--- Day8/AppFile.py
+++ Day8/AppFile.py
@@ -1,16 +1,21 @@
+import datetime
+file_name = "test.txt"
 conten_tosave = ["Hi today is wednes", "we tried numpy"]
-
-with open("test.txt", "w") as myFile:
-    myFile.write("starting to write\n")
+with open(file_name, "w") as myFile:
+    myFile.write(f"{datetime.datetime.now()}: starting to write\n")
     for line in conten_tosave:
-        myFile.write(line + "\n")
-
-with open("test.txt", "a") as myFile:
+        myFile.write(f"{datetime.datetime.now()}:{line}\n")
+with open(file_name, "a") as myFile:
     myFile.write("new line appended NOT REPLACED old one\n")
-
 try:
-    with open("test.txt", "r") as myFile:
+    with open(file_name, "r") as myFile:
         for line in myFile:
             print(line)
 except FileNotFoundError:
     print("file not found error")
+
+# class SaveToFile:
+#     def AddData(self, data): #file in append mode
+#         ...
+#     def UpdateData(self, data): # file in write mode
+#         ...
