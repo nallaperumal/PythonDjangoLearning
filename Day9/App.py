57a3a9f5c9f862731c97b6a1e2a083614374235e
Nalla <nallaperumal@gmail.com>
2026-02-04 18:24:15 +0530

    included bar chart

--- Day9/App.py
+++ Day9/App.py
@@ -3,7 +3,7 @@ import numpy as np
 
 days =  np.array([1,2,3,4,5,6,7,8,9,10,11,12])
 prices = np.array([350,400,410,435,451,489,500,350,310,329,331,350])
-plt.plot(days, prices,marker='o', color='green')
+plt.bar(days, prices,color='orange')
 plt.title("Simulated Silver Price")
 plt.xlabel("Day")
 plt.ylabel("Price ($)")
