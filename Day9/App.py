0757f0fb593ab25c5cdc1dfb25a5ab98445305c5
Nalla <nallaperumal@gmail.com>
2026-02-04 18:22:31 +0530

    included additional properties

--- Day9/App.py
+++ Day9/App.py
@@ -1,8 +1,11 @@
-
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt #pip install matplotlib
 import numpy as np
 
 days =  np.array([1,2,3,4,5,6,7,8,9,10,11,12])
-prices = np.array([11,22,31,14,51,16,17,28,29,10,31,12])
-plt.plot(days, prices)
+prices = np.array([350,400,410,435,451,489,500,350,310,329,331,350])
+plt.plot(days, prices,marker='o', color='green')
+plt.title("Simulated Silver Price")
+plt.xlabel("Day")
+plt.ylabel("Price ($)")
+plt.grid(True)
 plt.show()
