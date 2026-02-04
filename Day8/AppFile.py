06ccf717b9d2b12284ac684f32c06d47a7178976
Nalla <nallaperumal@gmail.com>
2026-02-04 15:50:41 +0530

    converted file write to a class

--- Day8/AppFile.py
+++ Day8/AppFile.py
@@ -1,21 +1,28 @@
 import datetime
-file_name = "test.txt"
-conten_tosave = ["Hi today is wednes", "we tried numpy"]
-with open(file_name, "w") as myFile:
-    myFile.write(f"{datetime.datetime.now()}: starting to write\n")
-    for line in conten_tosave:
-        myFile.write(f"{datetime.datetime.now()}:{line}\n")
-with open(file_name, "a") as myFile:
-    myFile.write("new line appended NOT REPLACED old one\n")
-try:
-    with open(file_name, "r") as myFile:
-        for line in myFile:
-            print(line)
-except FileNotFoundError:
-    print("file not found error")
 
-# class SaveToFile:
-#     def AddData(self, data): #file in append mode
-#         ...
-#     def UpdateData(self, data): # file in write mode
-#         ...
+class SaveToFile:
+    def __init__(self):
+        self.file_name = "test.txt"
+    def AddData(self, data): #file in append mode
+        with open(self.file_name, "a") as myFile:
+            myFile.write(data)
+    def UpdateData(self, data): # file in write mode
+        with open(self.file_name, "w") as myFile:
+            myFile.write(data)
+
+fileAcceesObj = SaveToFile()
+conten_tosave = ["Hi today is wednes", "we tried numpy"]
+fileAcceesObj.UpdateData(str(conten_tosave))
+fileAcceesObj.AddData("This data does not overrides but gets appended")
+
+# with open(file_name, "w") as myFile:
+#     myFile.write(f"{datetime.datetime.now()}: starting to write\n")
+#     for line in conten_tosave:
+#         myFile.write(f"{datetime.datetime.now()}:{line}\n")
+# try:
+#     with open(file_name, "r") as myFile:
+#         for line in myFile:
+#             print(line)
+# except FileNotFoundError:
+#     print("file not found error")
+
