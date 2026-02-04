b75df9fc315b3277ce3c05741165c301e01ab617
Nalla <nallaperumal@gmail.com>
2026-02-04 12:01:42 +0530

    added more numpy methods

--- Day8/AppNum.py
+++ Day8/AppNum.py
@@ -1,15 +1,23 @@
 #pip install numpy
 
 import numpy as np
-arr = np.array([1,2,3,4])
-print(np.__version__)
-print(arr)
-print(type(arr))
-arrT = np.array((1,2,5,6))
-print(arrT)
-print(type(arrT))
-arr2D = np.array([[1,2,3],[2,1,3]])
-arr4D = np.array([[[[1,2,3],[2,1,3]]]])
-print(arr4D)
-print(arr2D.ndim)
-print(arr4D.ndim)
+arr = np.array([1,2,3,4,5,6,7])
+newArr = arr.copy()
+newArr[3]= 25
+newView = arr.view()
+print(f"copy {newArr}")
+print(f"orig array:{arr}")
+newView[4] = 36
+print(f"(after view modification) orig array:{arr}")
+print(f"shape of array: {arr.shape}")
+# print(arr[0])
+# print(arr[1])
+# print(arr[1:])
+# print(arr[:2])
+# print(arr[::2])
+# print(arr[1::2]) #arr[<startIndex>:<EndIndex>:<steps>]
+arr = np.array([[1,2,3,4,5],[6,7,8,9,10]])
+print("shape:", arr.shape)
+# print(arr[1][3])
+# print(arr[1, 1:4])
+# print(arr[0, 3:5])
