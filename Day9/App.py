32fddb931f83a1994f312d74bd38d85cdc84eb89
Nalla <nallaperumal@gmail.com>
2026-02-04 19:35:38 +0530

    included data from pandas

--- Day9/App.py
+++ Day9/App.py
@@ -1,9 +1,18 @@
 import matplotlib.pyplot as plt #pip install matplotlib
 import numpy as np
+import pandas as pd
 
 days =  np.array([1,2,3,4,5,6,7,8,9,10,11,12])
+dayFrame = pd.DataFrame(days,columns=['Day'])
+
+print(dayFrame)
 prices = np.array([350,400,410,435,451,489,500,350,310,329,331,350])
-plt.bar(days, prices,color='orange')
+
+dayAndPrice = np.array([[1,350],[2,410],[3,450]])
+priceFrame = pd.DataFrame(dayAndPrice,columns=['Day','Price'])
+print(priceFrame)
+# plt.bar(days, prices,color='orange')
+plt.bar(priceFrame['Day'], priceFrame['Price'],color='orange')
 plt.title("Simulated Silver Price")
 plt.xlabel("Day")
 plt.ylabel("Price ($)")
