f9dcf7141b307f0c987b0e45e60f819c43bb6f67
Nalla <nallaperumal@gmail.com>
2026-02-04 18:10:08 +0530

    Basic visualization

---
+++ Day9/App.py
@@ -0,0 +1,8 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+days =  np.array([1,2,3,4,5,6,7,8,9,10,11,12])
+prices = np.array([11,22,31,14,51,16,17,28,29,10,31,12])
+plt.plot(days, prices)
+plt.show()
