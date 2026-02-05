7b911e3ede1faaa162ba3faf6239a9bc15401cca
Nalla <nallaperumal@gmail.com>
2026-02-05 11:26:31 +0530

    included pandas data

--- Day9/App.py
+++ Day9/App.py
@@ -1,42 +1,21 @@
-import matplotlib.pyplot as plt #pip install matplotlib
-import numpy as np
+#pip install pandas
 import pandas as pd
-
-days =  np.array([1,2,3,4,5,6,7,8,9,10,11,12])
+import numpy as np
+import matplotlib.pyplot as plt
+# pip install matplotlib
+days =  np.array([1,2,3,4,5])
 dayFrame = pd.DataFrame(days,columns=['Day'])
-
-# print(dayFrame)
-prices = np.array([350,400,410,435,451,489,500,350,310,329,331,350])
-
+print(dayFrame)
 dayAndPrice = np.array([[1,350],[2,410],[3,450]])
-priceFrame = pd.DataFrame(dayAndPrice,columns=['Day','Price'])
-# print("...")
-dataFrame = pd.read_csv('products-100.csv')
-new_df = dataFrame.dropna()
-# print(dataFrame.info())
-dataFrame.drop_duplicates(inplace = True)
-# print(dataFrame.info())
-# print(df.info())
-
-# df.dropna(inplace = True)
-# df.fillna(0,inplace = True)
-dataFrame.fillna({"Price": 0}, inplace=True)
-for x in dataFrame.index:
-  if dataFrame.loc[x, "Price"] > 600:
-    dataFrame.loc[x, "Price"] = 600
-salesByCategory = dataFrame.groupby(['Category','Brand'])['Price'].sum()    
-print(salesByCategory)
-salesByCategory.to_csv('products-100_updated.csv')
+dayAndPriceFrame = pd.DataFrame(dayAndPrice,columns=['Day', 'Price'])
 print("...")
-# print(new_df.to_string())
-
-# print(dataFrame[['Name','Price']])
-filteredDF = (dataFrame.loc[3:9,['Name','Price']])
-# filteredDF.plot()
-plt.bar(days, prices,color='orange')
-plt.bar(filteredDF['Name'], filteredDF['Price'],color='orange')
+print(dayAndPriceFrame)
+prices = np.array([220,250,310,350,240])
+# plt.plot(days, prices, marker='v', color='red')
+# plt.bar(days, prices,color='orange')
+plt.bar(dayAndPriceFrame['Day'], dayAndPriceFrame['Price'],color='orange')
 plt.title("Simulated Silver Price")
-plt.xlabel("Day")
-plt.ylabel("Price ($)")
+plt.xlabel("Days")
+plt.ylabel("price /g")
 plt.grid(True)
 plt.show()
