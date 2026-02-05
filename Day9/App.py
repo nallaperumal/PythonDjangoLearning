e947c8b229c14d9f0b1d8acc262b616f26d216a8
Nalla <nallaperumal@gmail.com>
2026-02-05 11:27:05 +0530

    removed the comments

--- Day9/App.py
+++ Day9/App.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-# pip install matplotlib
+
 days =  np.array([1,2,3,4,5])
 dayFrame = pd.DataFrame(days,columns=['Day'])
 print(dayFrame)
