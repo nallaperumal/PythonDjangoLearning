152a6781f1ac2cca6517c4eec1a4d358540bc2c7
Nalla <nallaperumal@gmail.com>
2026-02-09 17:53:43 +0530

    return the individual item as json

--- Day11/my_ecom/products/product_urls.py
+++ Day11/my_ecom/products/product_urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('products/', views.products, name='products'),
+    path('products/json/', views.product_json, name='product_json'),
+    path('products/json/<int:product_id>/', views.product_json_detail, 
+                name='product_json_detail'),
 ]
