dc02c580a10768e64341cd7bf7d5a6403b4463cb
Nalla <nallaperumal@gmail.com>
2026-02-09 15:20:23 +0530

    included images

--- Day11/my_ecom/products/views.py
+++ Day11/my_ecom/products/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 
 def products(request):
-    return HttpResponse("Hello.. Welcome to our products Page")
+    # return HttpResponse("Hello.. Welcome to our products Page")
+    myProducts = [
+        {'Name': 'Vanilla cake','Price': 300, 'Description': 'This is a vanilla cake'},
+        {'Name': 'Chocolate cake','Price': 320, 'Description': 'This is a chocolate cake'},
+         {'Name': 'Red Velvet cake','Price': 350, 'Description': 'This is a red velvet cake'}
+    ]
+    context = {
+        'myProducts': myProducts
+    }
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render(context, request))
