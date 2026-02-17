9f783b6e32e5dbaae2b2a404144aa2feba0bbfda
Nalla <nallaperumal@gmail.com>
2026-02-17 16:24:18 +0530

    included discount as an input parameter

--- AngularProj/my-new-angular/src/app/app.ts
+++ AngularProj/my-new-angular/src/app/app.ts
@@ -14,24 +14,7 @@ export class App {
   lang = 'English';
   imdb = 7.8;
   year_of_release = 2009;
-  price = 120;
-  num_of_tickets = 1;
+
   img = 'avatar.jpg';
-  computed_price = 120;
-  IsDiscounted = false;
-  dec_counter() {
-    if (this.num_of_tickets > 1)
-      this.num_of_tickets--;
-    this.computed_price = this.price * this.num_of_tickets;
-  }
-  inc_counter() {
-    this.num_of_tickets++;
-    console.log("entered the inc counter:"+this.num_of_tickets);
-    
-    this.computed_price = this.price * this.num_of_tickets;
-    if (this.num_of_tickets > 5) {
-      this.computed_price = this.computed_price * 0.9;
-      this.IsDiscounted = true;
-    }
-  }
+//  ng generate component <component-name>
 }
--- AngularProj/my-new-angular/src/app/ticket-counter/ticket-counter.ts
+++ AngularProj/my-new-angular/src/app/ticket-counter/ticket-counter.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-ticket-counter',
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './ticket-counter.css',
 })
 export class TicketCounter {
+  price = input<number>(120);
+  discount =  input<number>(0.9);
+  computed_price = 120;
+  IsDiscounted = false;
+  num_of_tickets = 1;
+  dec_counter() {
+    if (this.num_of_tickets > 1)
+      this.num_of_tickets--;
+    this.computed_price = this.price() * this.num_of_tickets;
+  }
+  inc_counter() {
+    this.num_of_tickets++;
+    console.log("entered the inc counter:" + this.num_of_tickets);
 
+    this.computed_price = this.price() * this.num_of_tickets;
+    if (this.num_of_tickets > 5) {
+      this.computed_price = this.computed_price * this.discount();
+      this.IsDiscounted = true;
+    }
+  }
 }
