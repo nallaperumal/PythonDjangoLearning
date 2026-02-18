206009784f1d6eea50e4125d4e4d3dc88cd81e22
Nalla <nallaperumal@gmail.com>
2026-02-18 12:20:16 +0530

    included the snack item image

--- AngularProj/my-new-angular/src/app/snack-item.ts
+++ AngularProj/my-new-angular/src/app/snack-item.ts
@@ -1,9 +1,11 @@
 export class SnackItem {
+    id: number;
     name: string;
     price: number;
     imageurl: string;
 
-    constructor(name: string, price: number, imageurl: string) {
+    constructor(id: number, name: string, price: number, imageurl: string) {
+        this.id = id;
         this.name = name;
         this.price = price;
         this.imageurl = imageurl;
