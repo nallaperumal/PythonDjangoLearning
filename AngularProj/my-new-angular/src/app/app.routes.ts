0e1278cd78ceb706f283f39198d85ca9c1a77c20
Nalla <nallaperumal@gmail.com>
2026-02-19 11:27:47 +0530

    included the menu

--- AngularProj/my-new-angular/src/app/app.routes.ts
+++ AngularProj/my-new-angular/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { TicketCounter } from './ticket-counter/ticket-counter';
+import { Popcorn } from './popcorn/popcorn';
+import { OtherMovies } from './other-movies/other-movies';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'ticket-counter', component: TicketCounter}, //http://localhost:4200/ticket-counter
+    {path: 'snacks', component: Popcorn},
+    {path: 'other-movies',component: OtherMovies},
+];
