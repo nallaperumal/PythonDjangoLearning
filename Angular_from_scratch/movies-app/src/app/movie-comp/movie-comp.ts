9b041cc2c1e891f465ce5bc7b0e93a4a0a5896f1
Nalla <nallaperumal@gmail.com>
2026-02-19 08:43:00 +0530

    included the signals

--- Angular_from_scratch/movies-app/src/app/movie-comp/movie-comp.ts
+++ Angular_from_scratch/movies-app/src/app/movie-comp/movie-comp.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MovieItem } from '../movie-item';
 import { MovieService } from '../movie-service';
 
@@ -9,12 +9,13 @@ import { MovieService } from '../movie-service';
   styleUrl: './movie-comp.css',
 })
 export class MovieComp {
-  movies: MovieItem[] = [];
+  movies = signal<MovieItem[]>([]);
 
   constructor(private movieService: MovieService) {
    this.movieService.getTop3Movies().subscribe(movies => {
-      this.movies = movies;
-      console.log(this.movies);
+      this.movies.set(movies);
+      console.log(this.movies());
+
     });
   }
 }
