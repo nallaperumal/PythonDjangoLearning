f9ca9d2509321e153cefafb86e55f691116c7fe9
Nalla <nallaperumal@gmail.com>
2026-02-19 08:51:37 +0530

    included add movies component

---
+++ Angular_from_scratch/movies-app/src/app/add-movie/add-movie.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-movie',
+  imports: [],
+  templateUrl: './add-movie.html',
+  styleUrl: './add-movie.css',
+})
+export class AddMovie {
+
+}
--- Angular_from_scratch/movies-app/src/app/app.routes.ts
+++ Angular_from_scratch/movies-app/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { MovieComp } from './movie-comp/movie-comp';
+import { AddMovie } from './add-movie/add-movie';
 
 export const routes: Routes = [
     {path: 'movie-comp', component: MovieComp}, // localhost:4200/movie-comp
+    {path: 'add-movie', component: AddMovie}, // localhost:4200/add-movie
 ];
