21f05de60532899111754cb32df43154d7cbee39
Nalla <nallaperumal@gmail.com>
2026-02-19 08:34:11 +0530

    implemented the service

--- Angular_from_scratch/movies-app/src/app/movie-comp/movie-comp.ts
+++ Angular_from_scratch/movies-app/src/app/movie-comp/movie-comp.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MovieItem } from '../movie-item';
+import { MovieService } from '../movie-service';
 
 @Component({
   selector: 'app-movie-comp',
@@ -7,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrl: './movie-comp.css',
 })
 export class MovieComp {
+  movies: MovieItem[] = [];
 
+  constructor(private movieService: MovieService) {
+   this.movieService.getTop3Movies().subscribe(movies => {
+      this.movies = movies;
+      console.log(this.movies);
+    });
+  }
 }
