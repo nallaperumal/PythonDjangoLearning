c6697f5c8af5e0815f80982425cc961f87719e0d
Nalla <nallaperumal@gmail.com>
2026-02-24 14:46:59 +0530

    Updated the UI of movie component Delete feature implemented Navigation from list to add movie

--- Angular_from_scratch/movies-app/src/app/movie-comp/movie-comp.ts
+++ Angular_from_scratch/movies-app/src/app/movie-comp/movie-comp.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { MovieItem } from '../movie-item';
 import { MovieService } from '../movie-service';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-comp',
@@ -9,16 +10,33 @@ import { MovieService } from '../movie-service';
   styleUrl: './movie-comp.css',
 })
 export class MovieComp {
-   movies = signal<MovieItem[]>([]);
+  movies = signal<MovieItem[]>([]);
 
-  constructor(private movieService: MovieService) {
-    console.log("before making the request...");    
-    this.movieService.getTop3Movies().subscribe(
+  deleteMovie(id: number) {
+    this.movieService.deleteMovie(id).subscribe(res => {
+      console.log("deleted");
+      this.fetchMovies();
+    }
+    )
+  }
+
+  gotoAddMoviePage() {
+    this.router.navigate(['add-movie']);
+  }
+
+  constructor(private movieService: MovieService, private router: Router) {
+    console.log("before making the request...");
+    this.fetchMovies();
+    console.log("after making the request...");
+  }
+
+  fetchMovies() {
+    this.movieService.getAllMovies().subscribe(
       movies => {
         console.log("received response from server...");
         this.movies.set(movies);
         console.log(this.movies());
       });
-      console.log("after making the request...");
   }
+
 }
