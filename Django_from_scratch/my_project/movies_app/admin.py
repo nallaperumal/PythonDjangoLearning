c536d81caec3ca514bb9a5c04ee33829c03f8bdd
Nalla <nallaperumal@gmail.com>
2026-03-04 11:37:27 +0530

    included through song artists through table to add more details to song

--- Django_from_scratch/my_project/movies_app/admin.py
+++ Django_from_scratch/my_project/movies_app/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
-from .models import Song,Movie
+from .models import Song,Movie, Person, Role,SongArtists
 
 admin.site.register(Song)
 admin.site.register(Movie)
+admin.site.register(Person)
+admin.site.register(Role)
+admin.site.register(SongArtists)
 # python manage.py createsuperuser
 
--- Django_from_scratch/my_project/movies_app/models.py
+++ Django_from_scratch/my_project/movies_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from rest_framework import serializers
 
+class Person(models.Model):
+    name = models.CharField(max_length=20)
+    state = models.CharField(max_length=20)
+
+class Role(models.Model):
+    name = models.CharField(max_length=20)
+    desc = models.CharField(max_length=20)    
     
 class Movie(models.Model):
     name = models.CharField(max_length=20)
@@ -12,13 +19,42 @@ class Movie(models.Model):
 class Song(models.Model):   
     name = models.CharField(max_length=20)
     running_time = models.FloatField()
-    singer = models.CharField(max_length=20)
+    # singer = models.CharField(max_length=20)
+    artists = models.ManyToManyField(Person, through='SongArtists')
     album = models.ForeignKey(Movie, on_delete=models.CASCADE, null=True, blank=True)
 
+# through table
+class SongArtists(models.Model):   
+    person = models.ForeignKey(Person, on_delete=models.CASCADE) #3
+    song = models.ForeignKey(Song, on_delete=models.CASCADE) # 2
+    role = models.ForeignKey(Role, on_delete=models.CASCADE) #1
+    class Meta:
+        unique_together = ('person', 'song', 'role') 
+
+class PersonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Person
+        fields = '__all__' 
+
+class RoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Role
+        fields = '__all__' 
+
+class SongArtistSerializer(serializers.ModelSerializer):  
+    person_name = serializers.ReadOnlyField(source ='person.name')  
+    role_name = serializers.ReadOnlyField(source ='role.name')    
+    class Meta:
+        model = SongArtists
+        fields = ['person_name','role_name']  
+
 class SongSerializer(serializers.ModelSerializer):
+    artists = SongArtistSerializer(many=True, read_only=True, source = 'songartists_set' )
     class Meta:
         model = Song
-        fields = '__all__'     
+        # fields = '__all__' 
+        fields = ['id','name','running_time', 'album', 'artists']    
+
 
 class MovieSerializer(serializers.ModelSerializer):
     songs = SongSerializer(many=True, read_only=True, source = 'song_set')
